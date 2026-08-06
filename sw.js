52e6232c4622a093f72fb0c607e607f755be0760
Labadimi <lavdrim.m@gjirafa.com>
2026-08-06 19:39:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Argati Service Worker v4
-const CACHE_NAME   = 'argati-v6.9.9.6';
+const CACHE_NAME   = 'argati-v6.9.9.7';
 const SCRIPT_URL   = 'https://script.google.com/macros/s/AKfycbzQMxhghzC2LCW36uaUJTlOI4WxHV6h8snnhRPRBgSM6fXeyG8LZS67Pzxoet41wes/exec';
 
 self.addEventListener('install',  e => { self.skipWaiting(); });
